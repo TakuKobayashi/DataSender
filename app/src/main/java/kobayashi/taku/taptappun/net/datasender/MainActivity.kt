074107c1e9e211b8205351f8878b7d7c4abc563f
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2018-08-10 13:38:08 +0900

    Bluetoothの設定していなかったら、ONにするダイアログを出す

--- app/src/main/java/kobayashi/taku/taptappun/net/datasender/MainActivity.kt
+++ app/src/main/java/kobayashi/taku/taptappun/net/datasender/MainActivity.kt
@@ -5,6 +5,7 @@ import android.bluetooth.BluetoothAdapter
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import android.content.Intent
 
 class MainActivity : Activity() {
 
@@ -20,6 +21,22 @@ class MainActivity : Activity() {
         if (adapter == null) {
             finish();
         }
+        // Bluetoothが有効になっていなかったらBluetoothを有効にするかどうか聞くようにする
+        if (!adapter.isEnabled()) {
+            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        // Bluetoothが有効になった場合のみアプリを継続させる
+        if(requestCode == REQUEST_ENABLE_BT){
+            if(resultCode != RESULT_OK){
+                finish();
+            }
+        }
     }
 
     /**
@@ -29,7 +46,7 @@ class MainActivity : Activity() {
     external fun stringFromJNI(): String
 
     companion object {
-
+        const val REQUEST_ENABLE_BT:Int = 1;
         // Used to load the 'native-lib' library on application startup.
         init {
             System.loadLibrary("native-lib")
